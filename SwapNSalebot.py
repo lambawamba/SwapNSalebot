b9a709cd9463a1fa71881427d4969f7035c470f3
Christopher Orosco <cjorosco9@gmail.com>
2016-04-08 14:39:53 -0500

    Update SwapNSalebot.py

--- SwapNSalebot.py
+++ SwapNSalebot.py
@@ -9,9 +9,9 @@ USERNAME  = ""
 #This is the bot's Username. In order to send mail, he must have some amount of Karma.
 PASSWORD  = ""
 #This is the bot's Password. 
-USERAGENT = "/u/(USERNAME GOES HERE) - History bot for /r/gameswap and /r/GameSale V.1. Creator - Lambawamba"
+USERAGENT = "/u/(USERNAME GOES HERE) - History bot for /r/gameswap and /r/GameSale V.1.02 Creator - Lambawamba"
 #This is a short description of what the bot does. For example "/u/GoldenSights' Newsletter bot"
-SUBREDDIT = "gameswap+GameSale"
+SUBREDDIT = "(subgoeshere)+(subgoeshere)"
 #This is the sub or list of subs to scan for new posts. For a single sub, use "sub1". For multiple subreddits, use "sub1+sub2+sub3+..."
 TITLESTRING = ["[H]", "[HAVE]", "(H)", "HAVE", "WTB", "WTS"]
 #These are the words you are looking for in the titles.
