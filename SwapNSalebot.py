93d845fd429bcf4560d7ae6f33e65ee3f58a80c8
Christopher Orosco <cjorosco9@gmail.com>
2021-03-03 13:51:25 -0600

    Update SwapNSalebot.py

--- SwapNSalebot.py
+++ SwapNSalebot.py
@@ -14,8 +14,7 @@ WAIT = 20
 COMMENT_TEMPLATE = """
 **It's dangerous to go alone! Take this.**
 
-
-[GameSale Rules](https://www.reddit.com/r/GameSale/wiki/index) | [Avoiding Scams] (https://www.reddit.com/r/GameSale/wiki/avoidscams) | [Universal Scammer List](https://universalscammerlist.com/search.php)
+[Avoiding Scams] (https://www.reddit.com/r/GameSale/wiki/avoidscams) | [Universal Scammer List](https://universalscammerlist.com/search.php)
 
 Username | Join date | Link karma | Comment karma
 :- | :-: | -: | -:
